c4fc4670b4697891969199f627512fbd4e9ffba4
DavidOrellana3 <david.orellana@dcmail.ca>
2025-04-01 23:22:46 -0400

    Update script.js

---
+++ practice/script.js
@@ -0,0 +1,20 @@
+<script>
+      function createParagraph() {
+        const para = document.createElement("p");
+        para.textContent = "you clicked the button!";
+        document.body.appendChild(para);
+      }
+
+      const buttons = document.querySelectorAll("button");
+
+
+
+
+
+      for (const button of buttons){
+        button.addEventListener("click",createParagraph);
+      }
+
+
+
+      </script>

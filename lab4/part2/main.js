71c3e69e91da4e1b4aceb89858ff8b772eee8c5e
DavidOrellana3 <david.orellana@dcmail.ca>
2025-04-04 15:47:51 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -9,8 +9,9 @@ const overlay = document.querySelector('.overlay');
 
 
 /* Declaring the alternative text for each image file */
-
+const imageFilenames = ["pic1.jpeg","pic2.jpeg","pic3.jpeg","pic4.jpeg","pic5.jpeg"]
 /* Looping through images */
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);

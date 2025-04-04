511c32e18933dffa838b68f8a8ad26d000abdc76
DavidOrellana3 <david.orellana@dcmail.ca>
2025-04-04 13:35:53 -0400

    Update main.js

--- lab4/part 1/main.js
+++ lab4/part 1/main.js
@@ -25,20 +25,20 @@ function result() {
     
 
     newStory = newStory.replace(":insertx:", xItem);
-newStory = newStory.replace(":inserty:", yItem);
-newStory = newStory.replace(":insertz:", zItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replace("Bob",name);
 
-    
-
   }
+
+  
   
   if(document.getElementById("uk").checked) {
     const weight = Math.round(300/14) + 'stone';
-    const temperature =  Math.round(94-30 * 5/9) + 'centigrade';
+    const temperature =  Math.round((94-32) * 5/9) + 'centigrade';
 
     newStory = newStory.replace('94 fahrenheit ', temperature);
     newStory = newStory.replace('300 pounds ', weight);
@@ -47,7 +47,7 @@ newStory = newStory.replace(":insertz:", zItem);
    
 
 
-
+  }
 
 
 

341c80b655163266bf644ac6de270dc7eac9741d
DavidOrellana3 <david.orellana@dcmail.ca>
2025-04-04 16:04:14 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -12,10 +12,17 @@ const overlay = document.querySelector('.overlay');
 const imageFilenames = ["pic1.jpeg","pic2.jpeg","pic3.jpeg","pic4.jpeg","pic5.jpeg"]
 /* Looping through images */
 
-
+filesname.forEach((filename,index) => {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+newImage.setAttribute('src', `images/${filename}`);
+newImage.setAttribute('alt', `Image ${index + 1}`);
 thumbBar.appendChild(newImage);
+});
+
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener("click",function(){
+    overlay.classList.toggle('active');
+
+
+});

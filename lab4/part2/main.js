c6abfbd8656e92da232ae1bc26e51308fe1029ac
DavidOrellana3 <david.orellana@dcmail.ca>
2025-04-04 20:55:10 -0400

    Update main.js

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -26,12 +26,12 @@ if (btn.getAttribute('class')==='dark') {
 
     btn.setAttribute('class','light');
     btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgb(0,0,0/50%)';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
 
 } else {
     btn.setAttribute('class','dark');
     btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgb(0,0,0/0%)';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
 
 }
 
